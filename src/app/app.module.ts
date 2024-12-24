706abcfa14de38c4efed70af3a459a6f82063512
radia idelkadi <idelkadiradia@gmail.com>
2024-12-24 20:19:28 +0100

    TS-17 Implementer la fonctionnalités de l'ajout d'une catégorie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -18,7 +18,8 @@ import { CategoryComponent } from './category/category.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
 
+  categories: string[] = ['Category name'];
+  ngOnInit() {
+
+  }
+
+  addCategory(category:string){
+    this.categories.push(category);
+  }
+  updateCategoryName(event: Event, index: number) {
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.categories[index] = inputValue;
+    console.log(this.categories);
+  }
 }
