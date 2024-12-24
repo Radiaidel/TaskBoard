5e9ae7913b73119e7a0af1520ac001002410ee90
radia idelkadi <idelkadiradia@gmail.com>
2024-12-24 18:22:59 +0100

    Ajouter les autres composants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {CategoryComponent} from "./category/category.component";
 
 const routes: Routes = [
-  // {path:"Home" , component: HomeComponent},
-  // {path: "Tasks" , component: TasksComponent}
+  {path:"Home" , component: DashboardComponent},
+  {path: "Tasks" , component: CategoryComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    DashboardComponent,
+    TasksComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
